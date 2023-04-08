639a3c13ed81cf9cd5dbf10b481797174392c3ab
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-04-09 02:54:18 +0300

    Addition has been done

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
-
-
 let gorevListesi = [
-    {id: 1, gorevAdi: "Görev 1"},
-    {id: 2, gorevAdi: "Görev 2"},
-    {id: 3, gorevAdi: "Görev 3"},
-    {id: 4, gorevAdi: "Görev 4"}
-    ]
+  { id: 1, gorevAdi: "Görev 1" },
+  { id: 2, gorevAdi: "Görev 2" },
+  { id: 3, gorevAdi: "Görev 3" },
+  { id: 4, gorevAdi: "Görev 4" },
+];
 
-let ul = document.getElementById("task-list")
+displayTask();
 
+function displayTask() {
+  let ul = document.getElementById("task-list");
+  ul.innerHTML = "";
 
+  for (let gorev of gorevListesi) {
+    let li = `
+                <li class="task list-group-item">
+                    <div class="form-check">
+                        <input type="checkbox" id="${gorev.id}" class="form-check-input">
+                        <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+                    </div>
+                </li>
+        `;
 
-for (let gorev of gorevListesi ){
-
-    let li =  `
-        <li class="task list-group-item">
-            <div class="form-check">
-                <input type="checkbox" id="${gorev.id}" class="form-check-input">
-                <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
-            </div>
-        </li>
-`
-
-ul.insertAdjacentHTML("afterbegin", li)
+    ul.insertAdjacentHTML("afterbegin", li);
+  }
 }
 
+function newTask(event) {
+  let taskInput = document.querySelector("#txtTaskName");
 
-
-
-
-
-function newTask(event){
-    console.log("click event: New Task")
-    console.log(event.target)
-    event.preventDefault()
+  if (taskInput.value == "") {
+    alert("Görev Alanı Boş Olamaz");
+  } else {
+    gorevListesi.push({
+      id: gorevListesi.length + 1,
+      gorevAdi: taskInput.value,
+    });
+    taskInput.value = "";
+    displayTask();
+  }
+  event.preventDefault();
 }
 
+let btnEkle = document
+  .querySelector("#btnAddNewTask")
+  .addEventListener("click", newTask);
 
+let btnSil = document
+  .querySelector("#btnClear")
+  .addEventListener("click", function (event) {
+    console.log("click event: Cleat Task");
 
-
-let btnEkle = document.querySelector("#btnAddNewTask").addEventListener("click", newTask)
-
-
-
-
-let btnSil = document.querySelector("#btnClear").addEventListener("click", function(event){
-    console.log("click event: Cleat Task")
-
-    event.preventDefault()
-})
-
-
-
-
+    event.preventDefault();
+  });
