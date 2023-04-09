16ec1948d25667a7999284e2edc40e39a690621c
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-04-09 06:42:10 +0300

    update
    I did a design update and fixed a bug that prevented us from adding elements back to the list when trying to delete all after editing.

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ let gorevListesi = [
   { id: 4, gorevAdi: "Görev 4" },
 ];
 
-let editId
-let isEditTask = false
+let editId;
+let isEditTask = false;
 
-let taskInput = document.querySelector("#txtTaskName");
+const taskInput = document.querySelector("#txtTaskName");
+const btnClear = document.querySelector("#btnClear");
 
 displayTask();
 
@@ -16,50 +17,56 @@ function displayTask() {
   let ul = document.getElementById("task-list");
   ul.innerHTML = "";
 
-  for (let gorev of gorevListesi) {
-    let li = `
-                <li class="task list-group-item">
-                    <div class="form-check">
-                        <input type="checkbox" id="${gorev.id}" class="form-check-input">
-                        <label for="${gorev.id}" class="form-check-label ">${gorev.gorevAdi}</label>
-                        
-                        <div class="dropdown float-end ">
-                            <button class="btn-sm btn btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fa-solid fa-ellipsis"></i>
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a onclick="deleteTask(${gorev.id})" href="#" class="dropdown-item"> <i class=" fa-solid fa-xmark "></i> Sil</a></li>
-                                <li><a onclick='editTask(${gorev.id}, "${gorev.gorevAdi}")' href="#" class="dropdown-item"> <i class="fa-solid fa-pencil"></i> Düzenle</a></li>
-                            </ul>
+  if (gorevListesi.length == 0) {
+    ul.innerHTML = '<h1 class="empty-task-list">Görev listeniz boş</h1>';
+  } else {
+    for (let gorev of gorevListesi) {
+      let li = `
+                    <li class="task list-group-item">
+                        <div class="form-check">
+                            <input type="checkbox" id="${gorev.id}" class="form-check-input">
+                            <label for="${gorev.id}" class="form-check-label ">${gorev.gorevAdi}</label>
+                            
+                            <div class="dropdown float-end ">
+                                <button class="btn-sm btn btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-ellipsis"></i>
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li>
+                                        <a onclick="deleteTask(${gorev.id})" href="#" class="dropdown-item"><i class="fas fa-trash-alt me-2"></i>Sil</a>
+                                    </li>
+                                    <li>
+                                        <a onclick='editTask(${gorev.id},"${gorev.gorevAdi}")' href="#" class="dropdown-item"><i class="fas fa-edit me-2"></i>Düzenle</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </li>
-        `;
+                    </li>
+            `;
 
-    ul.insertAdjacentHTML("afterbegin", li);
+      ul.insertAdjacentHTML("afterbegin", li);
+    }
   }
 }
 
 function newTask(event) {
-  
-
   if (taskInput.value == "") {
     alert("Görev Alanı Boş Olamaz");
   } else {
-      if(!isEditTask){
-                gorevListesi.push({
-                id: gorevListesi.length + 1,
-                gorevAdi: taskInput.value,
-            });
-      } else{
-            for(let gorev of gorevListesi){
-                if(gorev.id == editId){
-                    gorev.gorevAdi = taskInput.value
-                }
-                isEditTask = false 
-            }
-      }  
-      
+    if (!isEditTask) {
+      gorevListesi.push({
+        id: gorevListesi.length + 1,
+        gorevAdi: taskInput.value,
+      });
+    } else {
+      for (let gorev of gorevListesi) {
+        if (gorev.id == editId) {
+          gorev.gorevAdi = taskInput.value;
+        }
+      }
+      isEditTask = false;
+    }
+
     taskInput.value = "";
     displayTask();
   }
@@ -70,41 +77,34 @@ let btnEkle = document
   .querySelector("#btnAddNewTask")
   .addEventListener("click", newTask);
 
-
-
-
-
-
 let btnSil = document
   .querySelector("#btnClear")
   .addEventListener("click", function (event) {
-    console.log("click event: Cleat Task");
-
     event.preventDefault();
   });
 
+function deleteTask(id) {
+  let deleteID;
 
-  function deleteTask(id){
-
-    let deleteID
-
-    for(let index in gorevListesi){
-        if(gorevListesi[index].id == id){
-            deleteID = index
-        }
+  for (let index in gorevListesi) {
+    if (gorevListesi[index].id == id) {
+      deleteID = index;
     }
-
-    gorevListesi.splice(deleteID, 1)
-    displayTask()
   }
 
+  gorevListesi.splice(deleteID, 1);
+  displayTask();
+}
 
-  function editTask(taskId, taskName){
-    editId= taskId
-    isEditTask = true
-    taskInput.value = taskName
-    taskName.focus()
-    taskInput.classList.add("active")
+function editTask(taskId, taskName) {
+  editId = taskId;
+  isEditTask = true;
+  taskInput.value = taskName;
+  taskInput.focus();
+  taskInput.classList.add("active");
+}
 
-    
-  }
+btnClear.addEventListener("click", function () {
+  gorevListesi.splice(0, gorevListesi.length);
+  displayTask();
+});
